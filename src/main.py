d97ff201be71f7786bc2ee468a14fd4f7ff8bf08
Pepsisalvaje <EDSONPAREDESRAMIREZ12@GMAIL.COM>
2024-07-31 16:34:08 -0500

    upda te to the logic of the code

--- src/main.py
+++ src/main.py
@@ -1,40 +1,62 @@
 import argparse
 import requests
 from bs4 import BeautifulSoup
+import re
 
 def listsuid(lista):
     print("Revisando la Lista...")
 
-
-def uniquesuid(suid):
-    print(f"Revisando el SUID {suid}...")
-
+def findsuid(suid):
     url = f'https://gtfobins.github.io/gtfobins/{suid}/#sudo'
     
     # Realizar la solicitud a la página
-    print(url)
+    #print(url)
     response = requests.get(url)
 
     if response.status_code == 200:
         print(f"SUID {suid} is vulnerable")
+        print("\n")
         soup = BeautifulSoup(response.content, 'html.parser')
-        ul_examples = soup.find('ul', class_='examples')
+        h2 = soup.find('h2', id='sudo')
+        #ul_examples = soup.find('ul', class_='examples')
         
-        if ul_examples:
-        # Encontrar todos los elementos <code> dentro de <ul class="examples">
-            code_elements = ul_examples.find_all('code')
-        
-        for i, code_element in enumerate(code_elements, start=1):
-            code_text = code_element.text
-            print(f'Code {i}:\n{code_text}\n')
+        if h2:
+            # Buscar el siguiente hermano del h2 que sea un ul
+            ul = h2.find_next_sibling('ul')
+
+            
+
+            ul = str(ul)
+
+            ul = ul.replace('<ul class="examples">',"")
+            ul = ul.replace('<li>',"")
+            ul = ul.replace('<p>',"")
+            ul = ul.replace('</p>',"")
+            ul = ul.replace('</li>',"")
+            ul = ul.replace('</code>',"")
+            ul = ul.replace('</div>',"")
+            ul = ul.replace('</ul>',"")
+            ul = re.sub(r'<a\s+href="[^"]*">(.*?)</a>', r'\1' , ul, flags=re.DOTALL)
+            ul = re.sub(r'<div[^>]*>', '' , ul, flags=re.DOTALL)
+            ul = re.sub(r'<code[^>]*>', '' , ul, flags=re.DOTALL)
+            
+            #ul = re.sub(r'<p[^>]*>(.*?)</p>', r'\1', ul, flags=re.DOTALL)
+
+            #ul = re.sub(r'<a\s+code="[^"]*">(.*?)</code>', r'\1' , ul, flags=re.DOTALL)
+
+            print(ul)
+
+
         else:
-            print('No se encontró el elemento <ul class="examples">.')
+            print('No se encontró el elemento <h2> con id "7z".')
     else:
         print(f"SUID {suid} is not vulnerable")
 
+def uniquesuid(suid):
+    findsuid(suid)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Descripción de tu programa")
+    parser = argparse.ArgumentParser(description="Program Description")
 
     # Agregar argumentos
     parser.add_argument('-l', '--list', help='List of SUID')
